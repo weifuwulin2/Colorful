1a9fe016fd015f424a20bc59f9bc5c70ce3777d1
weifuwulin2 <41879472+weifuwulin2@users.noreply.github.com>
2025-11-05 19:04:03 +0200

    icon changes when type changes

--- Source/ColorMage/Private/ColorMageController.cpp
+++ Source/ColorMage/Private/ColorMageController.cpp
@@ -1,15 +1,95 @@
 #include "ColorMageController.h"
+
+#include "ColorableActor.h"
 #include "ColorManagerSubsystem.h"
 #include "GameFramework/Pawn.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
 #include "ColorMageCharacter.h"
+#include "ColorMagePlayerState.h"
 #include "PossessablePawn.h"
 #include "ColorSourceActor.h" 
 
-AColorMageController::AColorMageController() { bShowMouseCursor = false; }
+AColorMageController::AColorMageController()
+{
+	bShowMouseCursor = false;
+	PrimaryActorTick.bCanEverTick = true;
+}
+void AColorMageController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 
+	// 只在控制角色时才需要更新准星
+	if (!Cast<AColorMageCharacter>(GetPawn()))
+	{
+		// 如果不在控制角色 (比如在附身平台)，
+		// 确保准星变回 "None" 状态
+		if (CurrentTargetType != EReticleTargetType::None)
+		{
+			CurrentTargetType = EReticleTargetType::None;
+			OnReticleTargetChanged.Broadcast(CurrentTargetType);
+		}
+		return;
+	}
+
+	// 1. 设置射线
+	FVector CameraLocation;
+	FRotator CameraRotation;
+	GetPlayerViewPoint(CameraLocation, CameraRotation);
+	FVector TraceStart = CameraLocation;
+	// [!! 注意 !!] 我们使用一个合理的交互距离，而不是 10000 (射击距离)
+	// 否则你可能会瞄准到 100 米外的花
+	float ReticleTraceDistance = 15500.0f; // 20 米 (你可以在.h设为UPROPERTY)
+	FVector TraceEnd = TraceStart + (CameraRotation.Vector() * ReticleTraceDistance);
+
+	FHitResult HitResult;
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(GetPawn()); // 忽略玩家自己
+
+	EReticleTargetType NewTargetType = EReticleTargetType::None; // 默认为 "无"
+
+	// 2. 执行射线检测
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECC_Visibility, QueryParams))
+	{
+		AActor* HitActor = HitResult.GetActor();
+		if (HitActor)
+		{
+			// 3. 分析击中的物体 (优先级从高到低)
+
+			// 检查 1: 是否为可附身物体 (PossessablePawn)？
+			if (APossessablePawn* PossPawn = Cast<APossessablePawn>(HitActor))
+			{
+				AColorMagePlayerState* PS = GetPlayerState<AColorMagePlayerState>();
+				if (PS && PS->GetCurrentColor() != EColor::EC_None && PS->GetCurrentColor() == PossPawn->GetColor())
+				{
+					// 颜色匹配！最高优先级：可附身
+					NewTargetType = EReticleTargetType::Possessable;
+				}
+			}
+			// 检查 2: 是否为可上色的环境物体 (ColorableActor)？
+			else if (AColorableActor* ColorActor = Cast<AColorableActor>(HitActor))
+			{
+				// 它是灰色的，“可上色”
+				NewTargetType = EReticleTargetType::Paintable;
+			}
+			// 检查 3: 是否为颜色源 (ColorSourceActor)？
+			else if (AColorSourceActor* SourceActor = Cast<AColorSourceActor>(HitActor))
+			{
+				// “可汲取”
+				NewTargetType = EReticleTargetType::Extractable;
+			}
+			// (如果击中了其他东西，保持 NewTargetType = EReticleTargetType::None)
+		}
+	}
+
+	// 4. [!! 关键 !!] 仅在状态 *改变* 时才广播委托
+	if (NewTargetType != CurrentTargetType)
+	{
+		CurrentTargetType = NewTargetType;
+		OnReticleTargetChanged.Broadcast(CurrentTargetType);
+	}
+}
 void AColorMageController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
@@ -32,6 +112,8 @@ void AColorMageController::OnPossess(APawn* InPawn)
 		else if (APossessablePawn* PossPawn = Cast<APossessablePawn>(InPawn)) { PossessedType = PossPawn->GetControlType(); }
 	}
 	OnPawnControlChanged.Broadcast(PossessedType);
+	CurrentTargetType = EReticleTargetType::None;
+	OnReticleTargetChanged.Broadcast(CurrentTargetType);
 }
 
 void AColorMageController::SetupInputComponent()
--- Source/ColorMage/Public/ColorMageController.h
+++ Source/ColorMage/Public/ColorMageController.h
@@ -8,10 +8,30 @@
 #include "InputMappingContext.h"
 #include "ColorMageController.generated.h"
 
+UENUM(BlueprintType)
+enum class EReticleTargetType : uint8
+{
+	/** 默认状态，瞄准了无效物体 (墙壁、天空) */
+	None		UMETA(DisplayName = "None"),
+	
+	/** 瞄准了可汲取/混合的颜色源 (AColorSourceActor) */
+	Extractable	UMETA(DisplayName = "Extractable (Q键)"),
+
+	/** 瞄准了可上色的物体 (目标当前为灰色) */
+	Paintable	UMETA(DisplayName = "Paintable (LMB)"),
+
+	/** 瞄准了可附身的物体 (颜色已匹配) */
+	Possessable	UMETA(DisplayName = "Possessable (F键)"),
+	
+	/** 瞄准了“不可上色”的物体 (例如一个已上色的平台) */
+	Ineffective	UMETA(DisplayName = "Ineffective")
+};
+
 class AColorMageCharacter;
 /**
  * 
  */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnReticleTargetChanged, EReticleTargetType, NewTargetType);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPawnControlChanged, EPawnControlType, NewPawnType);
 UCLASS()
 class COLORMAGE_API AColorMageController : public APlayerController
@@ -19,6 +39,7 @@ class COLORMAGE_API AColorMageController : public APlayerController
 	GENERATED_BODY()
 public:
 	AColorMageController();
+	void Tick(float DeltaTime);
 	// ... (OnPawnControlChanged, RequestRepossessOriginalCharacter, InteractionDistance) ...
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FPawnControlChanged OnPawnControlChanged;
@@ -27,6 +48,8 @@ public:
 	float InteractionDistance = 10000.0f; // [!! 注意 !!] 汲取现在使用自己的距离
 	UPROPERTY()
 	TWeakObjectPtr<AColorMageCharacter> HiddenCharacter = nullptr;
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnReticleTargetChanged OnReticleTargetChanged;
 protected:
 	// ... (Input Actions: DefaultInputMappingContext, MoveAction, LookAction, AcquireAction, PossessAction) ...
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
@@ -56,4 +79,7 @@ private:
 	// ... (HiddenCharacter) ...
 	
 	friend class UColorManagerSubsystem;
+
+	UPROPERTY()
+	EReticleTargetType CurrentTargetType = EReticleTargetType::None;
 };
